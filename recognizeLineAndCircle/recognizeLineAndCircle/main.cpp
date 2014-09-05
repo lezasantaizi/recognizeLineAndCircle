d97c0003f1e7d85e58fb8d57af1e692267908aad
renxiaoming <xiaomingx.ren@intel.com>
2014-09-05 20:16:42 +0800

    add opencv success.

--- recognizeLineAndCircle/recognizeLineAndCircle/main.cpp
+++ recognizeLineAndCircle/recognizeLineAndCircle/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include "../3rdparty/include/opencv.hpp"
+
 
 using namespace std;
+using namespace cv;
 
+#pragma comment(lib, "opencv_highgui249d.lib")
+#pragma comment(lib, "opencv_imgproc249d.lib")
+#pragma comment(lib, "opencv_core249d.lib")
+#pragma comment(lib, "opencv_highgui249.lib")
+#pragma comment(lib, "opencv_imgproc249.lib")
+#pragma comment(lib, "opencv_core249.lib")
 int main()
 {
+	Mat temp = imread("hello.jpg");
+	namedWindow("hello");
+	imshow("hello",temp);
+	waitKey(0);
 	return 0;
 }
